6cbb2a638e93422163caff633d2c8f7a61eb7caa
Sushmita Kumari <63455991+sushmita24680@users.noreply.github.com>
2023-01-28 21:02:09 +0530

    added hourlyforcast feel_like and humidity

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ return res.json() ;
   
     
 }
-const formatTemperature = (temp) => `${temp?.toFixed(1)}`;
+const formatTemperature = (temp) => `${temp?.toFixed(1)}⁰`;
 
 const loadCurrentForcast = ({ main:{ temp_max, temp, temp_min, feels_like, humidity }, name,weather:[{description}] })=>{
 
@@ -24,15 +24,58 @@ const loadCurrentForcast = ({ main:{ temp_max, temp, temp_min, feels_like, humid
     console.log(temp, temp_max, temp_min, feels_like, humidity, description);
 };
 
-const loadHourlyForcast = async(city)=>{
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APi_Key}&units=metric`)
+const fetchHourlyForcast = async({name})=>{
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${name}&appid=${APi_Key}&units=metric`)
 
     const data = await res.json();
     console.log("forcast",data);
+    let i=1;
+    return data.list.map( forcast =>{
+       
+         const { main: { temp }, dt, dt_txt, weather: [{ icon }] } = forcast;
     
+        
+      i++;
+           return {temp,dt,dt_txt,icon,i};
+        
     
+       
+    
+      
+    })
 
 
+
+}
+
+const getIconURL =(icon)=>`http://openweathermap.org/img/wn/${icon}@2x.png`;
+const  loadHourlyForcast = (data)=>{
+    const currentDate = new Date();
+    console.log(currentDate,currentDate.getHours());
+    const forcast = document.querySelector(".hourly-forcast");
+    for(let d of data)
+    {
+    
+           forcast.innerHTML += `<article>
+                   <p>${d.dt_txt.split(" ")[1]}</p>
+                    <img class="icon" src="${getIconURL(d.icon)}" alt="image" />
+                    <p>${formatTemperature(d.temp)}</p>
+                </article>
+        
+        `;
+
+       }
+        
+   
+
+}
+
+const loadFeelsLike = ({main:{ feels_like }})=>{
+    document.querySelector("#feels-like p").innerHTML = feels_like;
+}
+
+const loadHumidity = ({main:{humidity}})=>{
+    document.querySelector("#humidity p").innerHTML = `${humidity}%`;
 }
 
 document.addEventListener("DOMContentLoaded",async()=>{
@@ -41,6 +84,9 @@ document.addEventListener("DOMContentLoaded",async()=>{
  const currentWeather = await getData(city);
  console.log(currentWeather);
 loadCurrentForcast(currentWeather);
-loadHourlyForcast(city);
+const hourlyForcast = await fetchHourlyForcast(currentWeather);
+    loadHourlyForcast(hourlyForcast);
+    loadFeelsLike(currentWeather);
+    loadHumidity(currentWeather);
 
 });
