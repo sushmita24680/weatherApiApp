8eba1c05ba4ed762d47c969647c9b51f62f3fca7
Sushmita Kumari <63455991+sushmita24680@users.noreply.github.com>
2023-01-28 19:11:35 +0530

    current weather has been set

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
 const APi_Key =`836ee4405e9598b55386cf309625c976`;
 
 
-  async function getData( city){
-     console.log("during fetch");
-    try{
-        let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APi_Key}`)
-        data = await res.json();
-        console.log("during fetch");
-        const { main: { temp_max, temp, temp_min, feels_like, humidity }, name } = data;
-        const { weather } = data;
-        const { description } = weather[0];
+const getData = async(city)=>{
+    
+const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APi_Key}&units=metric`)
 
-        const current = document.getElementById("current-forcast");
-        current.firstElementChild.textContent = name;
-        current.firstElementChild.nextElementSibling.textContent = temp;
-        console.log(data, temp_max, temp_min, feels_like, humidity, description);
-    }
-    catch{ e=> console.log(e.message);
-    }
+return res.json() ;
+
+     
+        // return data;
+  
     
 }
+const formatTemperature = (temp) => `${temp?.toFixed(1)}`;
 
-document.addEventListener("DOMContentLoaded",()=>{
-console.log("before fetch");
+const loadCurrentForcast = ({ main:{ temp_max, temp, temp_min, feels_like, humidity }, name,weather:[{description}] })=>{
+
+    const current =document.getElementById('current-forcast');
+    current.querySelector('.heading').textContent = name;
+    current.querySelector('.temp').textContent = formatTemperature(temp);
+    current.querySelector('.desc').textContent= description;
+    current.querySelector('.h-l').textContent =`High:${temp_max} Low:${temp_min}` 
+    console.log(temp, temp_max, temp_min, feels_like, humidity, description);
+};
+
+const loadHourlyForcast = async(city)=>{
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APi_Key}&units=metric`)
+
+    const data = await res.json();
+    console.log("forcast",data);
+    
+    
+
+
+}
+
+document.addEventListener("DOMContentLoaded",async()=>{
+
     const city = "pune";
-getData(city);
-
-console.log("after fetch");
-
+ const currentWeather = await getData(city);
+ console.log(currentWeather);
+loadCurrentForcast(currentWeather);
+loadHourlyForcast(city);
 
 });
