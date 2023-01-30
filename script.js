88efa85dd01f4a0bae1cae981df84f8e597dd2ce
Sushmita Kumari <63455991+sushmita24680@users.noreply.github.com>
2023-01-30 19:50:41 +0530

    added 5 days forcast layout

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const APi_Key =`836ee4405e9598b55386cf309625c976`;
 
-
+const week_of_Days = ['sun','mon','tue','wed','thu','fri','sat'];
+const minTemp = (temp)=>{
+    // console.log(Math.min(temp, 100));
+    return Math.min(temp, 100);
+};
 const getData = async(city)=>{
     
 const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APi_Key}&units=metric`)
@@ -29,14 +33,14 @@ const fetchHourlyForcast = async({name})=>{
 
     const data = await res.json();
     console.log("forcast",data);
-    let i=1;
+   
     return data.list.map( forcast =>{
        
          const { main: { temp }, dt, dt_txt, weather: [{ icon }] } = forcast;
     
         
-      i++;
-           return {temp,dt,dt_txt,icon,i};
+      
+           return {temp,dt,dt_txt,icon};
         
     
        
@@ -46,28 +50,50 @@ const fetchHourlyForcast = async({name})=>{
 
 
 
-}
 
+}
+const digit2Hour = (d) => { return new Date(d).toLocaleTimeString('en-us', { hour: "2-digit" }) }; 
 const getIconURL =(icon)=>`http://openweathermap.org/img/wn/${icon}@2x.png`;
 const  loadHourlyForcast = (data)=>{
     const currentDate = new Date();
-    console.log(currentDate,currentDate.getHours());
+    // console.log(currentDate,currentDate.getHours());
     const forcast = document.querySelector(".hourly-forcast");
+    data = data.slice(2,12);
+   
+    
     for(let d of data)
     {
-    
+        
            forcast.innerHTML += `<article>
-                   <p>${d.dt_txt.split(" ")[1]}</p>
+                   <p>${digit2Hour(d.dt_txt)}</p>
                     <img class="icon" src="${getIconURL(d.icon)}" alt="image" />
                     <p>${formatTemperature(d.temp)}</p>
                 </article>
-        
         `;
-
        }
-        
-   
+}
 
+const fiveDaysForcast = (data) =>{
+    console.log(data);
+    
+    for(let w in week_of_Days)
+    {
+       let f = data.filter(d =>{ 
+            if((new Date(d.dt_txt).getDay())=== +w)
+            {
+            // minTemp(d.temp);
+                return d;
+            }
+        
+        });
+        console.log(f);
+
+        let minTemp =100;
+        minTemp = f.filter(t => Math.min(t.temp,minTemp));
+        console.log(minTemp);
+    }
+    
+   
 }
 
 const loadFeelsLike = ({main:{ feels_like }})=>{
@@ -88,5 +114,6 @@ const hourlyForcast = await fetchHourlyForcast(currentWeather);
     loadHourlyForcast(hourlyForcast);
     loadFeelsLike(currentWeather);
     loadHumidity(currentWeather);
+    fiveDaysForcast(hourlyForcast);
 
 });
