887b31205737524f83e9120f30293b9c91c7e412
Sushmita Kumari <63455991+sushmita24680@users.noreply.github.com>
2023-01-31 14:29:38 +0530

    styled

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const minTemp = (temp)=>{
     // console.log(Math.min(temp, 100));
     return Math.min(temp, 100);
 };
+
+// const getCities = async()=>{
+
+// }
+
 const getData = async(city)=>{
     
 const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APi_Key}&units=metric`)
@@ -37,14 +42,8 @@ const fetchHourlyForcast = async({name})=>{
     return data.list.map( forcast =>{
        
         const { main: { temp ,temp_max, temp_min },  dt_txt, weather: [{ icon }] } = forcast;
-    
-        
       
            return {temp_max,temp_min,temp,dt_txt,icon};
-        
-    
-       
-    
       
     })
 
@@ -58,13 +57,13 @@ const  loadHourlyForcast = (data)=>{
     const currentDate = new Date();
     // console.log(currentDate,currentDate.getHours());
     const forcast = document.querySelector(".hourly-forcast");
-    data = data.slice(2,12);
-   
+    
+    data = data.slice(2, 12);
     
     for(let d of data)
     {
         
-           forcast.innerHTML += `<article>
+           forcast.innerHTML += `<article class="hours">
                    <p>${digit2Hour(d.dt_txt)}</p>
                     <img class="icon" src="${getIconURL(d.icon)}" alt="image" />
                     <p>${formatTemperature(d.temp)}</p>
@@ -74,24 +73,47 @@ const  loadHourlyForcast = (data)=>{
 }
 
 const fiveDaysForcast = (data) =>{
-    console.log(data);
+    // console.log(data);
+    // data = data.slice(2, -1);
     let dayWiseForecast = new Map();
-    let five_of_Days = week_of_Days.slice(+(new Date(data[0].dt_txt).getDay()), 6);
-    console.log("slice", five_of_Days);
-    for (let w in five_of_Days)
-    {
-        dayWiseForecast.set(five_of_Days[w],[...data.filter(d =>{ 
-            if((new Date(d.dt_txt).getDay()-1)=== +w)
-            {
-     
-                return d;
-            }
+    
+    // for (let w in week_of_Days)
+    // {
+    //     dayWiseForecast.set(week_of_Days[w],[...data.filter(d =>{ 
+    //         if((new Date(d.dt_txt).getDay())=== +w)
+    //         {
+                
+    //             return d;
+    //         }
         
-        })]);
+    //     })]);
      
 
-    }
+    // }
 
+    for(let d of data)
+    {
+             week_of_Days.filter((i,w)=>{
+             if((new Date(d.dt_txt).getDay())=== w)
+            {   
+                if(dayWiseForecast.has(i))
+                {
+                   let forcast = dayWiseForecast.get(i);
+                   forcast.push(d);
+                    
+                      dayWiseForecast.set(i,forcast);
+                }
+                else{
+                    dayWiseForecast.set(i,[d]);
+                }
+            }
+            
+        
+             })
+                
+            
+            
+    }
 
     console.log(dayWiseForecast);
     let miniTemp =0;
@@ -100,14 +122,20 @@ const fiveDaysForcast = (data) =>{
     {  
 
 
-        miniTemp = Math.min(...Array.from(value,val => val.temp_min));
-        // console.log(miniTemp);
-        maxTemp  = Math.max(...Array.from(value,val=>val.temp_max));
-    
-        
-        dayWiseForecast.set(key,{temp_max:miniTemp,temp_min:maxTemp,icon:value.find(v =>v.icon).icon})
-        
-        
+       if(value.length>0)
+       {
+           miniTemp = Math.min(...Array.from(value, val => val.temp_min));
+          
+           maxTemp = Math.max(...Array.from(value, val => val.temp_max));
+
+
+           dayWiseForecast.set(key, { temp_max: maxTemp, temp_min: miniTemp , icon: value.find(v => v.icon).icon })
+
+
+       }
+       else{
+        dayWiseForecast.delete(key);
+       }
          
     }
 
@@ -119,18 +147,18 @@ const fiveDaysForcast = (data) =>{
 const loadFiveDaysForcast = (data)=>{
     const dayWiseForecast = fiveDaysForcast(data);
     Array.from(dayWiseForecast).map(([days,{temp_max,temp_min,icon}],i)=>{
-        document.querySelector("#days-forcast").innerHTML+= ` 
+        if(i<5){
+            document.querySelector("#days-forcast").innerHTML += ` 
         <article class="days">
-          <h3>${i==0?"Today":days}</h3>
+          <h3>${i == 0 ? "Today" : days}</h3>
           <img height=70px src="${getIconURL(icon)}" alt="image" />
 
-          <p class="low">${formatTemperature(temp_min)}</p>
-          <p>${formatTemperature(temp_max)}</p>
+          <p class="low">${formatTemperature(temp_min)} L</p>
+          <p>${formatTemperature(temp_max)} H</p>
          </article>`;
+        }
     })
-    //    for ([key, value] of dayWiseForecast) {
-    
-    // }
+ 
 }
 
 const loadFeelsLike = ({main:{ feels_like }})=>{
@@ -143,6 +171,7 @@ const loadHumidity = ({main:{humidity}})=>{
 
 document.addEventListener("DOMContentLoaded",async()=>{
 
+    // const city = await getCities();
     const city = "pune";
  const currentWeather = await getData(city);
  console.log(currentWeather);
